5108c1b07253209a8f60e7c3cd3298439455bb2e
juliagmsouza <juliagmsouza12@gmail.com>
2023-12-09 16:12:06 -0300

    feat: finaliza implementação de backend

--- server.js
+++ server.js
@@ -1,3 +1,5 @@
+const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
 const bodyParser = require('body-parser')
 require('dotenv').config()
 const express = require('express')
@@ -21,7 +23,52 @@ const knex = require('knex')({
         ssl: { rejectUnauthorized: false },
     }
 });
-app.get('/albums', (req, res) => {
+
+let checkToken = (req, res, next) => {
+    let authToken = req.headers["authorization"]
+    if (!authToken) {
+        res.status(401).json({ message: 'Token de acesso requerida' })
+    }
+    else {
+        let token = authToken.split(' ')[1]
+        req.token = token
+    }
+    jwt.verify(req.token, process.env.SECRET_KEY, (err, decodeToken) => {
+        if (err) {
+            res.status(401).json({ message: 'Acesso negado' })
+            return
+        }
+        req.userId = decodeToken.id
+        next()
+    })
+}
+
+let isAdmin = (req, res, next) => {
+    knex
+        .select('*').from('user').where({ id: req.userId })
+        .then((users) => {
+            if (users.length) {
+                let user = users[0]
+                let roles = user.roles.split(';')
+                let adminRole = roles.find(i => i === 'ADMIN')
+                if (adminRole === 'ADMIN') {
+                    next()
+                    return
+                }
+                else {
+                    res.status(403).json({ message: 'Role de ADMIN requerida' })
+                    return
+                }
+            }
+        })
+        .catch(err => {
+            res.status(500).json({
+                message: 'Erro ao verificar roles de usuário - ' + err.message
+            })
+        })
+}
+
+app.get('/albums', checkToken, (req, res) => {
     knex.select('*').from('albums')
         .then(albums => res.status(200).json(albums))
         .catch(err => {
@@ -31,22 +78,22 @@ app.get('/albums', (req, res) => {
         })
 })
 
-app.post('/album', bodyParser.json(), (req, res) => {
+app.post('/album', bodyParser.json(), checkToken, isAdmin, (req, res) => {
     const album = {
         name: req.body.name,
         year: req.body.year,
         valor: req.body.valor,
     }
     knex('albums').returning('id').insert(album)
-    .then(id => res.status(201).json(id))
-    .catch(err => {
-        res.status(500).json({
-            message: 'Erro ao inserir album - ' + err.message
+        .then(id => res.status(201).json(id))
+        .catch(err => {
+            res.status(500).json({
+                message: 'Erro ao inserir album - ' + err.message
+            })
         })
-    })
 })
 
-app.get('/album/:id', (req, res) => {
+app.get('/album/:id', checkToken, (req, res) => {
     const id = Number(req.params.id)
     knex.select('*').from('albums').where('id', id)
         .then(album => res.status(200).json(album))
@@ -57,7 +104,7 @@ app.get('/album/:id', (req, res) => {
         })
 })
 
-app.put('/album/:id', bodyParser.json(), (req, res) => {
+app.put('/album/:id', bodyParser.json(), checkToken, isAdmin, (req, res) => {
     const id = Number(req.params.id)
     const album = {
         name: req.body.name,
@@ -73,7 +120,7 @@ app.put('/album/:id', bodyParser.json(), (req, res) => {
         })
 })
 
-app.delete('/album/:id', (req, res) => {
+app.delete('/album/:id', checkToken, isAdmin, (req, res) => {
     const id = Number(req.params.id)
     knex('albums').where('id', id).del()
         .then(album => res.status(200).json(album))
@@ -84,4 +131,57 @@ app.delete('/album/:id', (req, res) => {
         })
 })
 
-app.listen(port) 
+
+
+app.post('/security/register', (req, res) => {
+    knex('users')
+        .insert({
+            name: req.body.name,
+            login: req.body.login,
+            password: bcrypt.hashSync(req.body.password, 8),
+            email: req.body.email
+        }, ['id'])
+        .then((result) => {
+            let user = result[0]
+            res.status(200).json({ "id": user.id })
+            return
+        })
+        .catch(err => {
+            res.status(500).json({
+                message: 'Erro ao registrar usuario - ' + err.message
+            })
+        })
+})
+
+app.post('/security/login', (req, res) => {
+    knex
+        .select('*').from('users').where({ login: req.body.login })
+        .then(users => {
+            if (users.length) {
+                let user = users[0]
+                let checkPassword = bcrypt.compareSync(req.body.password, user.password)
+                if (checkPassword) {
+                    var tokenJWT = jwt.sign({ id: user.id },
+                        process.env.SECRET_KEY, {
+                        expiresIn: 3600
+                    })
+                    res.status(200).json({
+                        id: user.id,
+                        login: user.login,
+                        name: user.name,
+                        roles: user.roles,
+                        token: tokenJWT
+                    })
+                    return
+                }
+            }
+            res.status(200).json({ message: 'Login ou senha incorretos' })
+        })
+        .catch(err => {
+            res.status(500).json({
+                message: 'Erro ao verificar login - ' + err.message
+            })
+        })
+})
+app.listen(port)
+
